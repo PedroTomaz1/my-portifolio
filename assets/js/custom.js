cf7579a143dc60aa20c4bb7934ca6580f64e02a6
Kayck <kayckmatias04@gmail.com>
2023-02-04 15:08:06 -0300

    adicionado título dinâmico com javascript

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,21 @@
-document.title="My portfolio"
+var palavra = "Pedro Tomaz"
+
+var splitada = palavra.split('')
+
+document.title= " "
+
+x=0
+
+looping = setInterval(function () {
+    if(splitada[x] == ' '){
+        document.title += "\u205f​​​"
+        x++;
+    }else{
+        document.title += splitada[x++]
+    }
+
+    if(x > splitada.length){
+        document.title= " "
+        x=0
+    }
+}, 750);
